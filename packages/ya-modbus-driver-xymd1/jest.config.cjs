e63b9c9a7703150d95659cadbf5024bb2c81752b
Geno Roupsky <185580+groupsky@users.noreply.github.com>
2025-12-25 21:30:54 +0200

    feat(driver-xymd1): add temperature and humidity correction registers (#44)

--- packages/ya-modbus-driver-xymd1/jest.config.cjs
+++ packages/ya-modbus-driver-xymd1/jest.config.cjs
@@ -20,4 +20,12 @@ module.exports = {
     '!<rootDir>/src/**/*.test.ts',
     '!<rootDir>/src/**/*.d.ts',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 }
